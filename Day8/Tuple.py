e267d3a1c887ce4c17575dfec2b640925875982b
ketan soni <ketan504.ks@gmail.com>
2025-12-09 22:50:49 +0530

    completed tupe

--- Day8/Tuple.py
+++ Day8/Tuple.py
@@ -22,3 +22,27 @@
 # ...     print("I have green Tea")
 # ... 
 # I have green Tea
+# more_tea = ("Herbal","earl Grey ", "Herbal") 
+# >>> more_tea                                    
+# ('Herbal', 'earl Grey ', 'Herbal')
+# >>> more_tea.count("Herbal")
+# 2   
+# >>> more_tea.count("Her")    
+# 0
+# >>> tea_types = ('black','green','OOlang') 
+# >>> tea_types = ('Black','Green','Oolang') 
+# >>> (black,green,Oolang) = tea_types       
+# >>> Black
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# NameError: name 'Black' is not defined. Did you mean: 'black'?
+# >>> black
+# 'Black'
+# >>> type(tea_type)
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# NameError: name 'tea_type' is not defined. Did you mean: 'tea_types'?
+# >>> type(tea_types)
+# <class 'tuple'>
+# >>> ("",(1,2,4),"")
+('', (1, 2, 4), '')
